7820f41d45582a2f02702782dac9632e325703f1
eduardo.parolin <eduardo.parolin@tuntscorp.com>
2018-11-14 17:05:33 -0200

    fix4

--- pqp/ttt.js
+++ pqp/ttt.js
@@ -2,33 +2,10 @@ const moDb = require('./app/common/services/database.service');
 const filterDb = require('./app/common/services/filter.database.service');
 const logger = require('./app/common/services/log.service');
 
-async function compare(date) {
-    let moData = await getId(date);
-    for (let d of moData) {
-
-    }
-
-    if (data[0]) {
-        checkId(data[0].ID_MO_MOES)
-            .then((rData) => {
-                if (!rData[0]) {
-                    console.log('>>>>>>>>>>>>ID', data[0].ID_MO_MOES)
-                }
-            });
-        setTimeout(compare.bind(null, data[0].DT_ENVIO_MOES));
-    }
-}
-
 function getFirstF() {
     let query = 'select MIN(created_at) as min from messages where created_at BETWEEN \'2018-11-14\' AND \'2018-11-15\' ORDER BY created_at ASC LIMIT 1;';
     let params = [];
     return filterDb.runQuery(query, params, logger, 'getFirst');
-}
-
-function getFirst() {
-    let query = 'select MIN(DT_ENVIO_MOES) as min from MO_ESMS where DT_ENVIO_MOES BETWEEN \'2018-11-14\' AND \'2018-11-15\' AND CD_ROTA_MOES = 20000 ORDER BY DT_ENVIO_MOES ASC LIMIT 1;';
-    let params = [];
-    return moDb.runQuery(query, params, logger, 'getFirst');
 }
 
 function getIdFilter(date, counter) {
@@ -36,22 +13,25 @@ function getIdFilter(date, counter) {
     let params = [date];
     return filterDb.runQuery(query, params, logger, 'getIdFilter')
         .then(resp => {
-            for(let r of resp) {
-                //console.log(r);
-                checkId(r.id_mo)
+            let promises = []
+            for (let r of resp) {
+                promises.push(checkId(r.id_mo)
                     .then(moR => {
-                        if(!moR[0]) {
+                        if (!moR[0]) {
                             console.log('>>>>OOOPS', r.id_mo);
                         }
-                    })
-                    .catch(console.log)
+                        return Promise.resolve();
+                    }))
             }
-if(resp[0]){
-console.log(counter);            
-console.log(resp[resp.length-1].created_at);
-            setTimeout(getIdFilter.bind(null, resp[resp.length-1].created_at, ++counter));
-}        
-})
+            Promise.all(promises)
+                .then(_ => {
+                    if (resp[0]) {
+                        console.log(counter);
+                        console.log(resp[resp.length - 1].created_at);
+                        setTimeout(getIdFilter.bind(null, resp[resp.length - 1].created_at, ++counter));
+                    }
+                });
+        })
 }
 
 function checkId(id) {
